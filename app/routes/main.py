c4595789f3f8911dfca1ec1ffa8a0c28693156ce
brkcoc02 <88852782+brkcoc02@users.noreply.github.com>
2024-12-28 15:49:49 +0530

    Create main.py

---
+++ app/routes/main.py
@@ -0,0 +1,12 @@
+from flask import render_template, send_from_directory
+from app.routes import main_bp
+from app.utils.auth import requires_auth
+
+@main_bp.route('/')
+@requires_auth
+def serve_index():
+    return render_template('index.html')
+
+@main_bp.route('/favicon.ico')
+def serve_favicon():
+    return send_from_directory('static', 'favicon.ico')
